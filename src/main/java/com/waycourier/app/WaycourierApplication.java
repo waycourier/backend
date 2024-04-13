7549d074012d67bca33c2e3b6a9a0a4f94f51a28
kanchankk11 <nagkanchan5@gmail.com>
2024-04-13 17:08:29 +0530

    InitIAL Commit

---
+++ src/main/java/com/waycourier/app/WaycourierApplication.java
@@ -0,0 +1,13 @@
+package com.waycourier.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaycourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaycourierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waycourier/app/WaycourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waycourier.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaycourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
