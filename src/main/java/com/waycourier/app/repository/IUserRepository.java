6dd4515a5d1679998d1a2af8ae6d93a0fc578cae
Akash Saha <akash5@protonmail.com>
2024-04-23 03:09:41 +0000

    app now connects to mongodb container

--- src/main/java/com/waycourier/app/repository/IUserRepository.java
+++ src/main/java/com/waycourier/app/repository/IUserRepository.java
@@ -1,11 +1,12 @@
 package com.waycourier.app.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.waycourier.app.models.User;
 import java.util.List;
 
-
+@Repository
 public interface IUserRepository extends MongoRepository<User, String> {
 	List<User> findByUsername(String username);
 }
